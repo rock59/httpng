rock59/httpng

commit 096453f85efb033034a3fe4bece2928bab6f9489

add virtual host name to request information
Part of #3, #29

httpng/httpng.c
@@ -316,6 +316,7 @@ typedef struct {
 #endif /* SPLIT_LARGE_BODY */
 	int lua_handler_ref; /* Reference to user Lua handler. */
 	unsigned path_len;
+	unsigned authority_len;
 	unsigned query_at;
 	unsigned num_headers;
 	unsigned body_len;
@@ -1521,7 +1522,8 @@ fill_received_headers_and_body(lua_State *L, shuttle_t *shuttle)
 	assert(!response->sent_something);
 	const received_http_header_handle_t *const handles =
 		(received_http_header_handle_t *)&response->un.req.buffer[
-			response->un.req.path_len];
+			response->un.req.path_len +
+				response->un.req.authority_len];
 	const unsigned num_headers = response->un.req.num_headers;
 	lua_createtable(L, 0, num_headers);
 	unsigned current_offset = response->un.req.path_len + num_headers *
@@ -1882,13 +1884,16 @@ lua_fiber_func(va_list ap)
 	lua_rawgeti(L, LUA_REGISTRYINDEX, response->un.req.lua_handler_ref);
 
 	/* First param for Lua handler - req. */
-	lua_createtable(L, 0, 11);
+	lua_createtable(L, 0, 12);
 	lua_pushinteger(L, response->un.req.version_major);
 	lua_setfield(L, -2, "version_major");
 	lua_pushinteger(L, response->un.req.version_minor);
 	lua_setfield(L, -2, "version_minor");
 	lua_pushlstring(L, response->un.req.buffer, response->un.req.path_len);
 	lua_setfield(L, -2, "path");
+	lua_pushlstring(L, &response->un.req.buffer[response->un.req.path_len],
+		response->un.req.authority_len);
+	lua_setfield(L, -2, "host");
 	push_query(L, response);
 	lua_pushlstring(L, response->un.req.method,
 		response->un.req.method_len);
@@ -2019,7 +2024,32 @@ process_lua_req_in_tx(shuttle_t *shuttle)
 }
 #undef RETURN_WITH_ERROR
 
-/* Launched in HTTP server thread */
+/* Launched in HTTP server thread.
+ * Returns normalized authority ("virtual host" name) length. */
+static inline unsigned
+prepare_authority(h2o_req_t *req)
+{
+	assert(req->authority.len >= 0);
+	assert(req->authority.len <= INT_MAX);
+	unsigned authority_len = req->authority.len;
+	/* Formats: "foo.tarantool.io", "foo.tarantool.io:8443",
+	 * cut port number if present. */
+	int pos = authority_len - 1;
+	while (pos >= 0) {
+		const char c = req->authority.base[pos];
+		if (c == ':') {
+			authority_len = pos;
+			break;
+		}
+		if (c == '.')
+			/* Just optimization. */
+			break;
+		--pos;
+	}
+	return authority_len;
+}
+
+/* Launched in HTTP server thread. */
 static int
 lua_req_handler(lua_h2o_handler_t *self, h2o_req_t *req)
 {
@@ -2034,7 +2064,9 @@ lua_req_handler(lua_h2o_handler_t *self, h2o_req_t *req)
 		h2o_send_inline(req, H2O_STRLIT("Method name is too long\n"));
 		return 0;
 	}
-	if ((response->un.req.path_len = req->path.len) >
+	unsigned current_offset = response->un.req.path_len = req->path.len;
+	response->un.req.authority_len = prepare_authority(req);
+	if (current_offset + response->un.req.authority_len >
 	    conf.max_path_len_lua) {
 		/* Error. */
 		free_shuttle_with_anchor(shuttle);
@@ -2058,6 +2090,9 @@ lua_req_handler(lua_h2o_handler_t *self, h2o_req_t *req)
 		response->un.req.method_len);
 	memcpy(response->un.req.buffer, req->path.base,
 		response->un.req.path_len);
+	memcpy(&response->un.req.buffer[current_offset], req->authority.base,
+		response->un.req.authority_len);
+	current_offset += response->un.req.authority_len;
 
 	STATIC_ASSERT(LUA_QUERY_NONE <
 		(1ULL << (8 * sizeof(response->un.req.query_at))),
@@ -2069,11 +2104,10 @@ lua_req_handler(lua_h2o_handler_t *self, h2o_req_t *req)
 
 	const h2o_header_t *const headers = req->headers.entries;
 	const size_t num_headers = req->headers.size;
-	unsigned current_offset = response->un.req.path_len;
-
 	/* response->un.req.buffer[] format:
 	 *
-	 * char path[req->path.len]
+	 * char path[response->un.req.path.len]
+	 * char authority[response->un.req.authority_len]
 	 * FIXME: Alignment to at least to header_offset_t should be here
 	 *   (compatibility/performance).
 	 * received_http_header_handle_t handles[num_headers]

tests/all.lua
@@ -1352,3 +1352,53 @@ g_good_handlers.test_cancellation_http2_insecure = function()
     ensure_http2()
     test_cancellation('--http2')
 end
+
+local test_host = function(ver, use_tls)
+    local cfg = { handler = function(req) return {body = req.host} end }
+    local proto
+    if use_tls then
+        proto = 'https'
+        cfg.listen = {
+            tls = { ssl_pairs['foo'], ssl_pairs['bar'] },
+            uses_sni = true
+        }
+    else
+        proto = 'http'
+    end
+    http.cfg(cfg)
+
+    local cmd_foo = curl_bin .. ' -k -s ' .. ver .. ' ' .. proto ..
+        '://foo.tarantool.io:3300'
+    check_site_content(cmd_foo, 'foo.tarantool.io')
+
+    local cmd_bar = curl_bin .. ' -k -s ' .. ver .. ' ' .. proto ..
+        '://bar.tarantool.io:3300'
+    check_site_content(cmd_bar, 'bar.tarantool.io')
+
+    local cmd_localhost = curl_bin .. ' -k -s ' .. ver .. ' ' .. proto ..
+        '://localhost:3300'
+    if use_tls then
+        local ok, err = pcall(check_site_content, cmd_localhost, 'localhost')
+        assert(ok == false)
+    else
+        check_site_content(cmd_localhost, 'localhost')
+    end
+end
+
+g_good_handlers.test_host_http1_tls = function()
+    test_host('--http1.1', true)
+end
+
+g_good_handlers.test_host_http1_insecure = function()
+    test_host('--http1.1')
+end
+
+g_good_handlers.test_host_http2_tls = function()
+    ensure_http2()
+    test_host('--http2', true)
+end
+
+g_good_handlers.test_host_http2_insecure = function()
+    ensure_http2()
+    test_host('--http2')
+end

.github/workflows/with-tests.yml
@@ -51,6 +51,9 @@ jobs:
         cxx: 'clang++'
         cmake_defines: '-DENABLE_ASAN=ON'
 
+    - name: Add entries to /etc/hosts
+      run: sudo tests/mangle_etc_hosts.sh
+
     - name: Install luatest
       working-directory: ${{github.workspace}}/build
       # Ignore all errors (some are caused by ASAN in Tarantool)

tests/mangle_etc_hosts.sh
@@ -1,0 +1,2 @@
+echo '127.0.0.1 foo.tarantool.io' >>/etc/hosts
+echo '127.0.0.1 bar.tarantool.io' >>/etc/hosts

commit 47a6637703ff20258d88d38e721c464b343128c7

fix broken request headers and body
Address arithmetics was broken in
096453f85efb033034a3fe4bece2928bab6f9489
("add virtual host name to request information").

Added POST test which catches broken body.

httpng/httpng.c
@@ -1546,15 +1546,14 @@ fill_received_headers_and_body(lua_State *L, shuttle_t *shuttle)
 {
 	lua_response_t *const response = (lua_response_t *)&shuttle->payload;
 	assert(!response->sent_something);
+	unsigned current_offset = response->un.req.router_data_len +
+		response->un.req.path_len + response->un.req.authority_len;
 	const received_http_header_handle_t *const handles =
 		(received_http_header_handle_t *)&response->un.req.buffer[
-			response->un.req.router_data_len +
-			response->un.req.path_len +
-				response->un.req.authority_len];
+		current_offset];
 	const unsigned num_headers = response->un.req.num_headers;
 	lua_createtable(L, 0, num_headers);
-	unsigned current_offset = response->un.req.path_len + num_headers *
-		sizeof(received_http_header_handle_t);
+	current_offset += num_headers * sizeof(received_http_header_handle_t);
 	unsigned header_idx;
 	for (header_idx = 0; header_idx < num_headers; ++header_idx) {
 		const received_http_header_handle_t *const handle =

tests/all.lua
@@ -521,6 +521,10 @@ local empty_handler3 = function(req, io)
     return {body = ''}
 end
 
+local post_handler = function(req, io)
+    return { body = req.body }
+end
+
 local version_handler_launched = false
 local received_http1_req = false
 local received_http2_req = false
@@ -536,7 +540,7 @@ local check_http_version_handler = function(req, io)
     return {body = 'foo'}
 end
 
-local check_site_content = function(ver, proto, location, str, timeout)
+local check_site_content = function(extra, proto, location, str, timeout)
     ensure_popen()
     local target
     if using_popen() then
@@ -550,7 +554,7 @@ local check_site_content = function(ver, proto, location, str, timeout)
     else
         timeout_str = ''
     end
-    local cmd = curl_bin .. ' -k -s ' .. timeout_str .. ver .. target ..
+    local cmd = curl_bin .. ' -k -s ' .. timeout_str .. extra .. target ..
         proto .. '://' .. location
     local output
     if using_popen() then
@@ -1715,4 +1719,36 @@ end
 
 g_good_handlers.test_empty_response3_http2_tls = function()
     test_empty_response(empty_handler3, '--http2', true)
+end
+
+local test_post = function(ver, use_tls)
+    local cfg = {handler = post_handler}
+    local proto
+    if (use_tls) then
+        cfg.listen = listen_with_single_ssl_pair
+        proto = 'https'
+    else
+        proto = 'http'
+    end
+    http.cfg(cfg)
+
+    local test = 'The Matrix has you'
+    check_site_content(ver .. ' -d "' .. test .. '" ', proto,
+        'localhost:3300', test)
+end
+
+g_good_handlers.test_post_http1_tls = function()
+    test_post('--http2', true)
+end
+
+g_good_handlers.test_post_http2_tls = function()
+    test_post('--http1.1', true)
+end
+
+g_good_handlers.test_post_http1_insecure = function()
+    test_post('--http2')
+end
+
+g_good_handlers.test_post_http2_insecure = function()
+    test_post('--http1.1')
 end
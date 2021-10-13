3db8bf49d8eb7a7f02c864aee6386bc3cd20562a
Sergey Nikiforov <void@tarantool.org>
2021-10-13 12:48:50 +0300

    tests/examples: improve generation of key/cert
    Part of #231

--- tests/test_certs_helper.sh
+++ tests/test_certs_helper.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-openssl req -x509 -nodes -newkey rsa:2048 -subj /CN=localhost -subject -keyout $1/examples/key.pem -out $1/examples/cert.pem -days 200000
+
+SRC=$1
+CMD="openssl req -x509 -nodes -newkey rsa:2048 -subj /CN=localhost -subject -keyout $SRC/examples/key.pem -out $SRC/examples/cert.pem -days 200000"
+if [ ! -e $SRC/examples/key.pem ] || [ ! -e $SRC/examples/cert.pem ] ; then
+    $CMD
+else
+    echo "key.pem and cert.pem already exist"
+fi
